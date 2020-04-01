0b09a45a2ad831cc0eea53bc293fbbfbcb35bc43
Yury Shevchenko <shevchenko_yury@mail.ru>
2020-04-01 15:31:48 +0200

    remove token

--- components/Experiment/CustomDelete/index.js
+++ components/Experiment/CustomDelete/index.js
@@ -3,6 +3,7 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import { MY_PARAMETERS_QUERY } from '../../Experiments/Custom/my';
+import { ALL_PARAMETERS_QUERY } from '../../Experiments/Custom/index';
 
 const DELETE_PARAMETER_MUTATION = gql`
   mutation DELETE_PARAMETER_MUTATION($id: ID!) {
@@ -17,8 +18,6 @@ class DeleteCustomExperiment extends Component {
     // manually update the cache on the client so it matches the server
     // 1. read the cache
     const data = cache.readQuery({ query: MY_PARAMETERS_QUERY });
-    console.log('data', data);
-    console.log('payload', payload);
     // 2. Filter the deleted items out of the page
     data.myParameters = data.myParameters.filter(
       parameter => parameter.id !== payload.data.deleteParameter.id
@@ -33,6 +32,7 @@ class DeleteCustomExperiment extends Component {
         mutation={DELETE_PARAMETER_MUTATION}
         variables={{ id: this.props.id }}
         update={this.update}
+        refetchQueries={[{ query: ALL_PARAMETERS_QUERY }]}
       >
         {(deleteParameter, { error }) => (
           <button
