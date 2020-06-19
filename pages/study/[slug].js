f72e2eea45138819485fcd85c1eee71876b7789d
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-06-19 17:50:51 +0200

    updated packages 2

--- pages/study/[slug].js
+++ pages/study/[slug].js
@@ -7,12 +7,15 @@ const StudyLandingPage = props => (
   </Page>
 );
 
-export async function getStaticPaths() {
-  // Return a list of possible value for id
-}
+// make clean url for server-side rendering
+// https://nextjs.org/learn/basics/dynamic-routes
 
-export async function getStaticProps({ params }) {
-  // Fetch necessary data for the blog post using params.id
-}
+// export async function getStaticPaths() {
+//   // Return a list of possible value for id
+// }
+//
+// export async function getStaticProps({ params }) {
+//   // Fetch necessary data for the blog post using params.id
+// }
 
 export default StudyLandingPage;
