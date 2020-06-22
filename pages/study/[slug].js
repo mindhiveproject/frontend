edfa148e38b77f157e801ac4c398bf758579c5d4
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-06-22 18:01:23 +0200

    fallback

--- pages/study/[slug].js
+++ pages/study/[slug].js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import ReviewStudyForParticipants, {
   REVIEW_STUDY_QUERY,
 } from '../../components/Study/Landing/index';
@@ -6,74 +7,78 @@ import { endpoint, prodEndpoint } from '../../config';
 import { ALL_STUDIES_QUERY } from '../../components/Study/All/index';
 
 const url = process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint;
+// export default class StudyLandingPage extends React.Component {
+//   static async getInitialProps({ req, query }) {
+//     let data;
+//     if (process.browser) {
+//       console.log('I am in browser');
+//       data = await getStudyInfo(query.slug);
+//     } else {
+//       console.log('I am in server');
+//       data = await getStudyInfo(query.slug);
+//     }
+//     console.log('data', data);
+//     const study = (data && data.study) || {};
+//     // console.log('study', study);
+//     return { study };
+//   }
+//
+//   render() {
+//     // console.log('this.props', this.props);
+//     const { study } = this.props;
+//     if (study) {
+//       return (
+//         <Page>
+//           <ReviewStudyForParticipants study={study} />
+//         </Page>
+//       );
+//     }
+//     return (
+//       <Page>
+//         <div>No study found</div>
+//       </Page>
+//     );
+//   }
+// }
 
-export default class StudyLandingPage extends React.Component {
-  static async getInitialProps({ req, query }) {
-    let data;
-    if (process.browser) {
-      console.log('I am in browser');
-      data = await getStudyInfo(query.slug);
-    } else {
-      console.log('I am in server');
-      data = await getStudyInfo(query.slug);
-    }
-    console.log('data', data);
-    const study = (data && data.study) || {};
-    // console.log('study', study);
-    return { study };
+const StudyLandingPage = props => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
   }
-
-  render() {
-    // console.log('this.props', this.props);
-    const { study } = this.props;
-    if (study) {
-      return (
-        <Page>
-          <ReviewStudyForParticipants study={study} />
-        </Page>
-      );
-    }
-    return (
-      <Page>
-        <div>No study found</div>
-      </Page>
-    );
-  }
-}
-
-// const StudyLandingPage = props => (
-//   <Page>
-//     <ReviewStudyForParticipants study={props.study} />
-//   </Page>
-// );
-
+  return (
+    <Page>
+      <ReviewStudyForParticipants study={props.study} />
+    </Page>
+  );
+};
 // make clean url for server-side rendering
 // https://nextjs.org/learn/basics/dynamic-routes
 
-// async function getAllStudies() {
-//   const res = await fetch(url, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ query: ALL_STUDIES_QUERY }),
-//   });
-//   const json = await res.json();
-//   return json.data;
-// }
-//
-// export async function getStaticPaths() {
-//   const data = await getAllStudies();
-//   const { studies } = data;
-//   const paths = studies.map(study => ({
-//     params: {
-//       slug: study.slug,
-//     },
-//   }));
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-//
+async function getAllStudies() {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: ALL_STUDIES_QUERY }),
+  });
+  const json = await res.json();
+  return json.data;
+}
+
+export async function getStaticPaths() {
+  const data = await getAllStudies();
+  const { studies } = data;
+  const paths = studies.map(study => ({
+    params: {
+      slug: study.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
 async function getStudyInfo(slug) {
   const res = await fetch(url, {
     method: 'POST',
@@ -86,15 +91,15 @@ async function getStudyInfo(slug) {
   const json = await res.json();
   return json.data;
 }
-//
-// export async function getStaticProps({ params }) {
-//   const data = await getStudyInfo(params.slug);
-//   const { study } = data;
-//   return {
-//     props: {
-//       study,
-//     },
-//   };
-// }
 
-// export default StudyLandingPage;
+export async function getStaticProps({ params }) {
+  const data = await getStudyInfo(params.slug);
+  const { study } = data;
+  return {
+    props: {
+      study,
+    },
+  };
+}
+
+export default StudyLandingPage;
