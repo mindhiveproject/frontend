784a8fb33844e115ecc198e32e6fe1ca5fa37631
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-06-22 23:01:21 +0200

    weird soluttion3

--- pages/study/[slug].js
+++ pages/study/[slug].js
@@ -41,85 +41,85 @@ const url = process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint;
 //   }
 // }
 
-// const StudyLandingPage = () => {
-//   const router = useRouter();
-//   if (!router.query.slug)
-//     return (
-//       <Page>
-//         <p>Loading</p>
-//       </Page>
-//     );
-//   return (
-//     <Page>
-//       <ReviewStudyForParticipants slug={router.query.slug} />
-//     </Page>
-//   );
-// };
-
-const StudyLandingPage = props => {
-  // const router = useRouter();
-  if (!props || !props.study) {
+const StudyLandingPage = () => {
+  const router = useRouter();
+  if (!router.query.slug)
     return (
       <Page>
-        <h1>Not working</h1>
+        <p>Loading</p>
       </Page>
     );
-  }
   return (
     <Page>
-      <ReviewStudyForParticipants study={props.study} />
+      <ReviewStudyForParticipants slug={router.query.slug} />
     </Page>
   );
 };
 
+// const StudyLandingPage = props => {
+//   // const router = useRouter();
+//   if (!props || !props.study) {
+//     return (
+//       <Page>
+//         <h1>Not working</h1>
+//       </Page>
+//     );
+//   }
+//   return (
+//     <Page>
+//       <ReviewStudyForParticipants study={props.study} />
+//     </Page>
+//   );
+// };
+
 // make clean url for server-side rendering
 // https://nextjs.org/learn/basics/dynamic-routes
 
-async function getAllStudies() {
-  const res = await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: ALL_STUDIES_QUERY }),
-  });
-  const json = await res.json();
-  return json.data;
-}
-
-export async function getStaticPaths() {
-  const data = await getAllStudies();
-  const { studies } = data;
-  const paths = studies.map(study => ({
-    params: {
-      slug: study.slug,
-    },
-  }));
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-async function getStudyInfo(slug) {
-  const res = await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query: REVIEW_STUDY_QUERY,
-      variables: { slug },
-    }),
-  });
-  const json = await res.json();
-  return json.data;
-}
-
-export async function getStaticProps({ params }) {
-  const data = await getStudyInfo(params.slug);
-  const { study } = data;
-  return {
-    props: {
-      study,
-    },
-  };
-}
+// async function getAllStudies() {
+//   const res = await fetch(url, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ query: ALL_STUDIES_QUERY }),
+//   });
+//   const json = await res.json();
+//   return json.data;
+// }
+//
+// export async function getStaticPaths() {
+//   const data = await getAllStudies();
+//   const { studies } = data;
+//   const paths = studies.map(study => ({
+//     params: {
+//       slug: study.slug,
+//     },
+//   }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+//
+// async function getStudyInfo(slug) {
+//   const res = await fetch(url, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//       query: REVIEW_STUDY_QUERY,
+//       variables: { slug },
+//     }),
+//   });
+//   const json = await res.json();
+//   return json.data;
+// }
+//
+// export async function getStaticProps({ params }) {
+//   const data = await getStudyInfo(params.slug);
+//   const { study } = data;
+//   return {
+//     props: {
+//       study,
+//     },
+//   };
+// }
 
 export default StudyLandingPage;
