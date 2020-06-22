13910cd82710fc94b96f36f7b136a2deb65e698a
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-06-22 22:25:53 +0200

    last trial

--- components/Study/Landing/index.js
+++ components/Study/Landing/index.js
@@ -26,28 +26,28 @@ const REVIEW_STUDY_QUERY = gql`
   }
 `;
 
-// class ReviewStudyForParticipants extends Component {
-//   render() {
-//     return <StudyParticipantPage study={this.props.study} />;
-//   }
-// }
-//
 class ReviewStudyForParticipants extends Component {
   render() {
-    return (
-      <Query query={REVIEW_STUDY_QUERY} variables={{ slug: this.props.slug }}>
-        {({ error, loading, data }) => {
-          if (error) return <Error error={error} />;
-          if (loading) return <p>Loading</p>;
-          if (!data.study) return <p>No study found for {this.props.slug}</p>;
-          const { study } = data;
-          console.log('study', study);
-          return <StudyParticipantPage study={study} />;
-        }}
-      </Query>
-    );
+    return <StudyParticipantPage study={this.props.study} />;
   }
 }
+//
+// class ReviewStudyForParticipants extends Component {
+//   render() {
+//     return (
+//       <Query query={REVIEW_STUDY_QUERY} variables={{ slug: this.props.slug }}>
+//         {({ error, loading, data }) => {
+//           if (error) return <Error error={error} />;
+//           if (loading) return <p>Loading</p>;
+//           if (!data.study) return <p>No study found for {this.props.slug}</p>;
+//           const { study } = data;
+//           console.log('study', study);
+//           return <StudyParticipantPage study={study} />;
+//         }}
+//       </Query>
+//     );
+//   }
+// }
 
 export default ReviewStudyForParticipants;
 export { REVIEW_STUDY_QUERY };
--- pages/study/[slug].js
+++ pages/study/[slug].js
@@ -1,10 +1,12 @@
 import { useRouter } from 'next/router';
-import ReviewStudyForParticipants from '../../components/Study/Landing/index';
+import ReviewStudyForParticipants, {
+  REVIEW_STUDY_QUERY,
+} from '../../components/Study/Landing/index';
 import Page from '../../components/Page/index';
-// import { endpoint, prodEndpoint } from '../../config';
-// import { ALL_STUDIES_QUERY } from '../../components/Study/All/index';
+import { endpoint, prodEndpoint } from '../../config';
+import { ALL_STUDIES_QUERY } from '../../components/Study/All/index';
 
-// const url = process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint;
+const url = process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint;
 // export default class StudyLandingPage extends React.Component {
 //   static async getInitialProps({ req, query }) {
 //     let data;
@@ -39,84 +41,85 @@ import Page from '../../components/Page/index';
 //   }
 // }
 
-const StudyLandingPage = () => {
-  const router = useRouter();
-  if (!router.query.slug)
-    return (
-      <Page>
-        <p>Loading</p>
-      </Page>
-    );
-  return (
-    <Page>
-      <ReviewStudyForParticipants slug={router.query.slug} />
-    </Page>
-  );
-};
-
-// const StudyLandingPage = props => {
+// const StudyLandingPage = () => {
 //   const router = useRouter();
-//   if (!router.query.study)
+//   if (!router.query.slug)
 //     return (
 //       <Page>
-//         <h1>Not working</h1>
+//         <p>Loading</p>
 //       </Page>
 //     );
 //   return (
 //     <Page>
-//       <ReviewStudyForParticipants study={props.study} />
+//       <ReviewStudyForParticipants slug={router.query.slug} />
 //     </Page>
 //   );
 // };
 
+const StudyLandingPage = props => {
+  // const router = useRouter();
+  if (!props || !props.study) {
+    return (
+      <Page>
+        <h1>Not working</h1>
+      </Page>
+    );
+  }
+  return (
+    <Page>
+      <ReviewStudyForParticipants study={props.study} />
+    </Page>
+  );
+};
+
 // make clean url for server-side rendering
 // https://nextjs.org/learn/basics/dynamic-routes
 
-// async function getAllStudies() {
-//   const res = await fetch(url, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ query: ALL_STUDIES_QUERY }),
-//   });
-//   const json = await res.json();
-//   return json.data;
-// }
-//
-// export async function getStaticPaths() {
-//   const data = await getAllStudies();
-//   const { studies } = data;
-//   const paths = studies.map(study => ({
-//     params: {
-//       slug: study.slug,
-//     },
-//   }));
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-//
-// async function getStudyInfo(slug) {
-//   const res = await fetch(url, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({
-//       query: REVIEW_STUDY_QUERY,
-//       variables: { slug },
-//     }),
-//   });
-//   const json = await res.json();
-//   return json.data;
-// }
-//
-// export async function getStaticProps({ params }) {
-//   const data = await getStudyInfo(params.slug);
-//   const { study } = data;
-//   return {
-//     props: {
-//       study,
-//     },
-//   };
-// }
+async function getAllStudies() {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: ALL_STUDIES_QUERY }),
+  });
+  const json = await res.json();
+  return json.data;
+}
+
+export async function getStaticPaths() {
+  const data = await getAllStudies();
+  const { studies } = data;
+  const paths = studies.map(study => ({
+    params: {
+      slug: study.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+async function getStudyInfo(slug) {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: REVIEW_STUDY_QUERY,
+      variables: { slug },
+    }),
+  });
+  const json = await res.json();
+  return json.data;
+}
+
+export async function getStaticProps({ params }) {
+  const data = await getStudyInfo(params.slug);
+  const { study } = data;
+  return {
+    props: {
+      study,
+    },
+  };
+}
 
 export default StudyLandingPage;
