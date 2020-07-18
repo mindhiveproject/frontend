6aee7381014bada5a79cf370c43b096ee288c896
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-07-18 13:19:46 +0200

    pass reset2

--- pages/reset/index.js
+++ pages/reset/index.js
@@ -1,10 +1,26 @@
+import { useRouter } from 'next/router';
 import Reset from '../../components/Reset/index';
 import Page from '../../components/Page/index';
 
-const ResetPage = ({ query }) => (
-  <Page>
-    <Reset resetToken={query.resetToken} />
-  </Page>
-);
+// const ResetPage = ({ query }) => (
+//   <Page>
+//     <Reset resetToken={query.resetToken} />
+//   </Page>
+// );
+
+const ResetPage = () => {
+  const router = useRouter();
+  if (!router.query.resetToken)
+    return (
+      <Page>
+        <p>Loading</p>
+      </Page>
+    );
+  return (
+    <Page>
+      <Reset resetToken={router.query.resetToken} />
+    </Page>
+  );
+};
 
 export default ResetPage;
--- pages/study/[slug].js
+++ pages/study/[slug].js
@@ -1,45 +1,6 @@
 import { useRouter } from 'next/router';
-import ReviewStudyForParticipants, {
-  REVIEW_STUDY_QUERY,
-} from '../../components/Study/Landing/index';
+import ReviewStudyForParticipants from '../../components/Study/Landing/index';
 import Page from '../../components/Page/index';
-import { endpoint, prodEndpoint } from '../../config';
-// import { ALL_STUDIES_QUERY } from '../../components/Study/Board/all';
-
-const url = process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint;
-// export default class StudyLandingPage extends React.Component {
-//   static async getInitialProps({ req, query }) {
-//     let data;
-//     if (process.browser) {
-//       console.log('I am in browser');
-//       data = await getStudyInfo(query.slug);
-//     } else {
-//       console.log('I am in server');
-//       data = await getStudyInfo(query.slug);
-//     }
-//     console.log('data', data);
-//     const study = (data && data.study) || {};
-//     // console.log('study', study);
-//     return { study };
-//   }
-//
-//   render() {
-//     // console.log('this.props', this.props);
-//     const { study } = this.props;
-//     if (study) {
-//       return (
-//         <Page>
-//           <ReviewStudyForParticipants study={study} />
-//         </Page>
-//       );
-//     }
-//     return (
-//       <Page>
-//         <div>No study found</div>
-//       </Page>
-//     );
-//   }
-// }
 
 const StudyLandingPage = () => {
   const router = useRouter();
@@ -55,71 +16,5 @@ const StudyLandingPage = () => {
     </Page>
   );
 };
-
-// const StudyLandingPage = props => {
-//   // const router = useRouter();
-//   if (!props || !props.study) {
-//     return (
-//       <Page>
-//         <h1>Not working</h1>
-//       </Page>
-//     );
-//   }
-//   return (
-//     <Page>
-//       <ReviewStudyForParticipants study={props.study} />
-//     </Page>
-//   );
-// };
-
-// make clean url for server-side rendering
-// https://nextjs.org/learn/basics/dynamic-routes
-
-// async function getAllStudies() {
-//   const res = await fetch(url, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ query: ALL_STUDIES_QUERY }),
-//   });
-//   const json = await res.json();
-//   return json.data;
-// }
-//
-// export async function getStaticPaths() {
-//   const data = await getAllStudies();
-//   const { studies } = data;
-//   const paths = studies.map(study => ({
-//     params: {
-//       slug: study.slug,
-//     },
-//   }));
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-//
-// async function getStudyInfo(slug) {
-//   const res = await fetch(url, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({
-//       query: REVIEW_STUDY_QUERY,
-//       variables: { slug },
-//     }),
-//   });
-//   const json = await res.json();
-//   return json.data;
-// }
-//
-// export async function getStaticProps({ params }) {
-//   const data = await getStudyInfo(params.slug);
-//   const { study } = data;
-//   return {
-//     props: {
-//       study,
-//     },
-//   };
-// }
 
 export default StudyLandingPage;
