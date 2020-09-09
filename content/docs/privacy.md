7a240af8ede9f8375d95224c7abfea1b894f3af1
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-09-10 00:00:37 +0200

    redirect cms1

--- content/docs/privacy.md
+++ content/docs/privacy.md
@@ -1,13 +1,13 @@
 ---
 title: Privacy policy
 date: 2020-03-07T23:41:14.705Z
-body:
-  <div>
-    <h1>
-      Preamble
-    </h1>
-  </div>
-  <div>
-    With the following privacy policy we would like to inform you which types of your personal data (hereinafter also abbreviated as "data") we process for which purposes and in which scope. The privacy statement applies to all processing of personal data carried out by us, both in the context of providing our services and in particular on our websites, in mobile applications and within external online presences, such as our social media profiles (hereinafter collectively referred to as "online services").
-  </div>
 ---
+
+<div>
+  <h1>
+    Preamble
+  </h1>
+</div>
+<div>
+  With the following privacy policy we would like to inform you which types of your personal data (hereinafter also abbreviated as "data") we process for which purposes and in which scope. The privacy statement applies to all processing of personal data carried out by us, both in the context of providing our services and in particular on our websites, in mobile applications and within external online presences, such as our social media profiles (hereinafter collectively referred to as "online services").
+</div>
--- pages/docs/[slug].js
+++ pages/docs/[slug].js
@@ -1,32 +1,56 @@
 import React, { Component } from 'react';
+import fs from 'fs';
 import Page from '../../components/Page/index';
 import Documents from '../../components/Documents/index';
 
-class Document extends Component {
-  static async getInitialProps({ query }) {
-    const { slug } = query;
+export const getStaticPaths = async () => {
+  const files = fs.readdirSync('content/docs');
+  console.log('files', files);
+  const paths = files.map(filename => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }));
+  console.log('paths', paths);
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-    const document = await import(`../../content/docs/${slug}.md`).catch(
-      error => null
-    );
+export const getStaticProps = async ({ params: { slug } }) => ({
+  props: {
+    slug,
+  },
+});
 
-    return { document };
-  }
+const Document = ({ slug }) => <div>Hello {slug}</div>;
 
-  render() {
-    // if (!this.props.document) return <div>Document not found</div>;
-    console.log('this.props.document', this.props.document);
-
-    const {
-      attributes: { date, title, body },
-    } = this.props.document.default;
-
-    return (
-      <Page>
-        <Documents date={date} title={title} body={body} />
-      </Page>
-    );
-  }
-}
+// class Document extends Component {
+//   static async getInitialProps({ query }) {
+//     const { slug } = query;
+//
+//     const document = await import(`../../content/docs/${slug}.md`).catch(
+//       error => null
+//     );
+//
+//     return { document };
+//   }
+//
+//   render() {
+//     // if (!this.props.document) return <div>Document not found</div>;
+//     console.log('this.props.document', this.props.document);
+//
+//     const {
+//       attributes: { date, title, body },
+//     } = this.props.document.default;
+//
+//     return (
+//       <Page>
+//         <Documents date={date} title={title} body={body} />
+//       </Page>
+//     );
+//   }
+// }
 
 export default Document;
