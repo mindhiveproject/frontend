b007b26fa6f13459ddada51f2ecd15e6a8eb994f
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-11-30 21:17:16 +0100

    share class results

--- pages/classes/results.js
+++ pages/classes/results.js
@@ -1,10 +1,25 @@
+import { useRouter } from 'next/router';
 import ClassResults from '../../components/Class/Results/index';
 import Page from '../../components/Page/index';
+import { PageOnlyFor } from '../../components/Permissions/General/index';
 
-const ClassResultsPage = ({ query }) => (
-  <Page>
-    <ClassResults id={query.id} />
-  </Page>
-);
+const ClassResultsPage = () => {
+  const router = useRouter();
+  if (!router.query.id)
+    return (
+      <PageOnlyFor roles={['TEACHER', 'ADMIN']}>
+        <Page>
+          <p>Loading ...</p>
+        </Page>
+      </PageOnlyFor>
+    );
+  return (
+    <Page>
+      <PageOnlyFor roles={['TEACHER', 'ADMIN']}>
+        <ClassResults id={router.query.id} />
+      </PageOnlyFor>
+    </Page>
+  );
+};
 
 export default ClassResultsPage;
