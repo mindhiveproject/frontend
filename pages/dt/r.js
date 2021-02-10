4aff245af217bfb40fe711088ea1866b4500f674
Yury Shevchenko <shevchenko_yury@mail.ru>
2021-02-10 13:27:35 +0100

    router queries

--- pages/dt/r.js
+++ pages/dt/r.js
@@ -6,8 +6,8 @@ const RunTestPage = ({ query: { t, s } }) => {
   // t is a test id, s is a study id
   const router = useRouter();
   // if either the test or study id are missing, return the error message
-  if (!(router.query.t && router.query.s)) return <h1>The link is invalid!</h1>;
-  return <UserWrapper t={t} s={s} />;
+  if (!(router.query.t && router.query.s)) return <p>Loading</p>;
+  return <UserWrapper t={router.query.t} s={router.query.s} />;
 };
 
 RunTestPage.propTypes = {
