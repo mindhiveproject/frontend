3ec799d7f6dfca7917de9c771aa9835d1b9c7d73
Yury-Shevchenko <shevchenko_yury@mail.ru>
2021-10-14 16:58:19 +0200

    show homework on student tab 2

--- components/Dashboard/Classes/ClassPage/StudentPage/homework.js
+++ components/Dashboard/Classes/ClassPage/StudentPage/homework.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-
 import { Accordion, Icon } from 'semantic-ui-react';
 
+import styled from 'styled-components';
 import Homework from '../../Homework/homework';
+
+const EmptyRow = styled.div`
+  display: grid;
+  padding: 10px;
+  grid-template-columns: 1fr;
+  background: white;
+  grid-gap: 1rem;
+  text-align: center;
+  align-content: center;
+  height: 100%;
+`;
 
 class StudentHomework extends Component {
   state = {};
@@ -18,24 +29,32 @@ class StudentHomework extends Component {
   render() {
     const { activeIndex } = this.state;
 
+    if (this.props?.student?.authorOfHomework?.length > 0) {
+      return (
+        <Accordion>
+          {this.props.student.authorOfHomework.map((homework, id) => (
+            <>
+              <Accordion.Title
+                active={activeIndex === id}
+                index={id}
+                onClick={this.handleClick}
+              >
+                <Icon name="dropdown" />
+                {homework?.title}
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === id}>
+                <Homework homeworkId={homework?.id} />
+              </Accordion.Content>
+            </>
+          ))}
+        </Accordion>
+      );
+    }
+
     return (
-      <Accordion>
-        {this.props.student.authorOfHomework.map((homework, id) => (
-          <>
-            <Accordion.Title
-              active={activeIndex === id}
-              index={id}
-              onClick={this.handleClick}
-            >
-              <Icon name="dropdown" />
-              {homework?.title}
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === id}>
-              <Homework homeworkId={homework?.id} />
-            </Accordion.Content>
-          </>
-        ))}
-      </Accordion>
+      <EmptyRow>
+        <div>The student hasn’t written any homework yet.</div>
+      </EmptyRow>
     );
   }
 }
