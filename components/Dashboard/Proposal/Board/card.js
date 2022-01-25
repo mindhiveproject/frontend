4f8115fdb89b520ff2ce4654b0f1214b6d1aca9f
Yury-Shevchenko <shevchenko_yury@mail.ru>
2022-01-25 16:41:20 +0100

    remove console log

--- components/Dashboard/Proposal/Board/card.js
+++ components/Dashboard/Proposal/Board/card.js
@@ -25,7 +25,6 @@ class Card extends Component {
       case 'Closed':
         statusStyle = 'status-closed';
     }
-    console.log('statusStyle', statusStyle);
 
     return (
       <Draggable key={card.id}>
