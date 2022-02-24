54e27c75f125280d7840a293eecf44bcb6734063
Sam Hill <sam.hudhill@gmail.com>
2022-02-23 16:40:30 -0800

    authenticated users routed directly to dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import AllStudies from '../components/Study/Board/public';
 import Page from '../components/Page/index';
+import AuthorizedPage from '../components/Page/userpage';
+import { Query } from '@apollo/client/react/components';
+import { USER_DASHBOARD_QUERY } from '../components/User/index';
+import Router from 'next/router';
 
 const AllStudiesPage = props => (
-  <Page>
-    <AllStudies />
-  </Page>
+  <Query query={USER_DASHBOARD_QUERY}>
+      {({ data, loading }) => {
+        if (loading) return <p>Loading</p>;
+        if (!data?.me) 
+          return (
+            <Page>
+              <AllStudies />
+            </Page>
+          );
+          Router.push({
+            pathname: '/dashboard',
+          });
+          return <AuthorizedPage />
+      }}
+  </Query>
 );
 
 export default AllStudiesPage;
