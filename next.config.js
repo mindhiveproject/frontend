9241d202791eaf3493616df9b558162ab7776800
Yury-Shevchenko <shevchenko_yury@mail.ru>
2022-04-21 08:50:24 +0200

    removed history

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
-      config.node = {
-        ...config.node,
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        child_process: 'empty',
-      };
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.tls = false;
+      config.resolve.fallback.child_process = false;
     }
+
+    // if (!isServer) {
+    //   config.node = {
+    //     ...config.node,
+    //     fs: 'empty',
+    //     net: 'empty',
+    //     tls: 'empty',
+    //     child_process: 'empty',
+    //   };
+    // }
 
     config.module.rules.push({
       test: /\.md$/,
