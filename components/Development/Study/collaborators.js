1b02444e0c542e1b5a9377f8679ee7ec664457c6
Sam Hill <sam.hudhill@gmail.com>
2022-05-11 10:46:16 -0700

    installed avatar package

--- components/Development/Study/collaborators.js
+++ components/Development/Study/collaborators.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Image, Dropdown, Icon } from 'semantic-ui-react';
+import Avatar from 'react-avatar';
 
 class Collaborators extends Component {
 
@@ -13,70 +14,56 @@ class Collaborators extends Component {
           borderRadius: '50px/60px',
           width: '120px',
           height: '32px',
-          padding: '2px'
+          padding: '3px'
         }}
       >
-          <span 
+          <Avatar // can have a googleID attribute that you pass it
+            name="Laurie Anderson" 
+            size="26px" 
+            round={true}
             style={{
-              border: '1px solid white',
-              borderRadius: '50%',
-              padding: '6px',
               position: 'absolute',
               left: '50%',
-              backgroundColor: 'red',
-              width: '28px',
-              height: '28px'
             }}
-          >
-            <span>SH</span>
-          </span>
-          <span
+          /> 
+          <Avatar
+            name="Joe Shmo"
+            size="26px"
+            round={true}
             style={{
-              border: '1px solid white',
-              borderRadius: '50%',
-              padding: '6px',
               position: 'absolute',
               left: '35%',
-              backgroundColor: 'blue',
-              width: '28px',
-              height: '28px',
             }}
-          >
-            <span>LM</span>
-          </span>
-          <span
+          />
+          <Avatar
+            name="Sam Hill"
+            size="26px"
+            round={true}
             style={{
-              border: '1px solid white',
-              borderRadius: '50%',
-              padding: '6px',
               position: 'absolute',
               left: '20%',
-              backgroundColor: 'yellow',
-              width: '28px',
-              height: '28px'
             }}
-          >
-            RN
-          </span>
+          />
           <span
             style={{
-              border: '1px solid white',
               borderRadius: '50%',
               padding: '6px',
               position: 'absolute',
               left: '5%',
               backgroundColor: 'grey',
-              width: '28px',
-              height: '28px'
+              width: '26px',
+              height: '26px',
+              fontSize: '.8em',
+              fontWeight: 'bold' //not working???
             }}
           >
-          <span // number is adaptive to # of collaborators
+          <span
             style={{
               fontSize: 'small'
             }}
           > 
             <Dropdown
-              trigger='+3'
+              trigger='+3' // this number is a reflection of how many > 3 collaborators there are
               icon={null}
               floating
               simple // causes the dropdown to trigger on hover rather than click
@@ -84,7 +71,7 @@ class Collaborators extends Component {
               <Dropdown.Menu>
                 <Dropdown.Item>List of additional collaborators</Dropdown.Item>
               </Dropdown.Menu>
-              {/* the dropdown displays additional collaborators */}
+              {/* the dropdown displays the additional collaborators */}
             </Dropdown>
           </span>
           </span>
