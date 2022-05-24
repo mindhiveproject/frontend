be874a2adc3371855a5243a61107b85121d01c0e
Sam Hill <sam.hudhill@gmail.com>
2022-05-24 16:00:10 -0700

    need a query

--- components/Development/Study/collaborators.js
+++ components/Development/Study/collaborators.js
@@ -1,99 +1,102 @@
 import React, { Component } from 'react';
+import { Query } from '@apollo/client/react/components';
+import gql from 'graphql-tag';
 import { Image, Dropdown, Icon } from 'semantic-ui-react';
 import Avatar from 'react-avatar';
 
 class Collaborators extends Component {
-
 	render() {
+    const {study} = this.props;
+    const existingCollaborators = study?.collaborators || [];
   	return (
-
-      <div 
-        style={{
-          position: 'relative',
-          backgroundColor: '#DEDEDE',
-          borderRadius: '50px/60px',
-          width: '120px',
-          height: '32px',
-          padding: '3px'
-        }}
-      >
-          <Avatar // can have a googleID attribute that you pass it
-            name="Laurie Anderson" 
-            size="26px" 
-            round={true}
+          <div 
             style={{
-              position: 'absolute',
-              left: '50%',
-            }}
-          /> 
-          <Avatar
-            name="Joe Shmo"
-            size="26px"
-            round={true}
-            style={{
-              position: 'absolute',
-              left: '35%',
-            }}
-          />
-          <Avatar
-            name="Sam Hill"
-            size="26px"
-            round={true}
-            style={{
-              position: 'absolute',
-              left: '20%',
-            }}
-          />
-          <span
-            style={{
-              borderRadius: '50%',
-              padding: '6px',
-              position: 'absolute',
-              left: '5%',
-              backgroundColor: 'grey',
-              width: '26px',
-              height: '26px',
-              fontSize: '.8em',
-              fontWeight: 'bold' //not working???
+              position: 'relative',
+              backgroundColor: '#DEDEDE',
+              borderRadius: '50px/60px',
+              width: '120px',
+              height: '32px',
+              padding: '3px'
             }}
           >
-          <span
-            style={{
-              fontSize: 'small'
-            }}
-          > 
-            <Dropdown
-              trigger='+3' // this number is a reflection of how many > 3 collaborators there are
-              icon={null}
-              floating
-              simple // causes the dropdown to trigger on hover rather than click
-            >
-              <Dropdown.Menu>
-                <Dropdown.Item>List of additional collaborators</Dropdown.Item>
-              </Dropdown.Menu>
-              {/* the dropdown displays the additional collaborators */}
-            </Dropdown>
-          </span>
-          </span>
-          <span
-            style={{
-              position: 'absolute',
-              left: '75%',
-              padding: '6px'
-            }}
-          >
-            <Icon 
-              name='dropdown'
+            <Avatar // can have a googleID attribute that you pass it
+              name="Laurie Anderson" 
+              googleId=""
+              size="26px" 
+              round={true}
               style={{
-                cursor: 'pointer'
+                position: 'absolute',
+                left: '50%',
               }}
-              onClick={() => {
-                this.props.openSharingModal();
-              }} 
+            /> 
+            <Avatar
+              name="Joe Shmo"
+              size="26px"
+              round={true}
+              style={{
+                position: 'absolute',                  
+                left: '35%',
+              }}
             />
-          </span>
-      </div>
-  	);
+            <Avatar
+              name="Sam Hill"
+              size="26px"
+              round={true}
+              style={{
+                position: 'absolute',                  
+                left: '20%',
+              }}
+            />
+            <span
+              style={{
+                borderRadius: '50%',
+                padding: '6px',
+                position: 'absolute',
+                left: '5%',                  
+                backgroundColor: 'grey',
+                width: '26px',
+                height: '26px',
+                fontSize: '.8em',
+                fontWeight: 'bold' //not working???
+              }}
+            >
+              <span
+                style={{
+                  fontSize: 'small'                
+                }}
+              > 
+                <Dropdown
+                  trigger='+3' // this number is a reflection of how many > 3 collaborators there are
+                  icon={null}
+                  floating
+                  simple // causes the dropdown to trigger on hover rather than click
+                >                  
+                  <Dropdown.Menu>
+                    <Dropdown.Item>List of additional collaborators</Dropdown.Item>
+                  </Dropdown.Menu>
+                  {/* the dropdown displays the additional collaborators */}
+                </Dropdown>
+              </span>
+            </span>
+            <span
+              style={{
+                position: 'absolute',
+                left: '75%',
+                padding: '6px'
+              }}
+            >
+              <Icon 
+                name='dropdown'
+                style={{
+                  cursor: 'pointer'
+                }}
+                onClick={() => {
+                  this.props.openSharingModal();
+                }} 
+              />
+            </span>
+          </div>
+    );
 	}
 }
 
