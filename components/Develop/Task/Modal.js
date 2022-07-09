a8eacb4efe31c84c3d432cf7763365bd30b9beca
Sam Hill <sam.hudhill@gmail.com>
2022-07-09 13:50:46 -0700

    styling to match Kim's

--- components/Develop/Task/Modal.js
+++ components/Develop/Task/Modal.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react';
-import { Modal, Icon } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Modal, Icon } from "semantic-ui-react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledContent = styled.div`
   display: grid;
   grid-template-columns: 70% auto;
-  grid-gap: 20px;
-  padding: 50px;
+  column-gap: 1px;
+  .leftPanel {
+    padding 70px;
+    background-color: #F7F9F8;
+  }
+  .rightPanel {
+    padding: 50px;
+    background-color: #F7F9F8;
+  }
+  .contentBlock {
+    margin-bottom: 50px;
+  }
   .symbolBlock {
-    background: #F7F9F8;
+    background: #FFFFFF;
     padding: 10px;
     max-width: 300px;
+    font-size: 16px;
+    margin-bottom: 50px;
     div {
       padding 5px;
     }
+  }
+  p, li {
+    font-size: 16px;
   }
 `;
 
 const StyledHeader = styled.div`
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: 70% auto;
   grid-gap: 20px;
   padding: 50px;
   .rightPanel {
     padding-top: 50px;
-    margin-left: 50px;
   }
 `;
 
@@ -66,10 +80,9 @@ const StyledButtons = styled.div`
   }
 `;
 
-class CollaboratorsModal extends Component {
+class TaskModal extends Component {
   render() {
     const { component } = this.props;
-    console.log({ component });
 
     return (
       <Modal
@@ -116,39 +129,39 @@ class CollaboratorsModal extends Component {
             </div>
           </StyledHeader>
         </Modal.Header>
-        <Modal.Content>
+        <Modal.Content style={{ padding: "0px", backgroundColor: "#E6E6E6" }}>
           <StyledContent>
-            <div>
+            <div className="leftPanel">
               <h2>Parameters</h2>
               <p>The following features of this task can be tweaked:</p>
               <div className="symbolBlock">
                 <div>
-                  <Icon name="clipboard outline" style={{ color: '#556AEB' }} />
+                  <Icon name="clipboard outline" style={{ color: "#556AEB" }} />
                   Task Instructions
                 </div>
                 <div>
-                  <Icon name="clone outline" style={{ color: '#556AEB' }} />
+                  <Icon name="clone outline" style={{ color: "#556AEB" }} />
                   Number of trials
                 </div>
                 <div>
-                  <Icon name="star outline" style={{ color: '#556AEB' }} />
+                  <Icon name="star outline" style={{ color: "#556AEB" }} />
                   Number of points participants start the task with
                 </div>
                 <div>
-                  <Icon name="random" style={{ color: '#556AEB' }} />
+                  <Icon name="random" style={{ color: "#556AEB" }} />
                   Whether trials should be randomized
                 </div>
                 <div>
                   <Icon
                     name="question circle outline"
-                    style={{ color: '#556AEB' }}
+                    style={{ color: "#556AEB" }}
                   />
                   Which question is asked between trials and how often
                 </div>
                 <div>
                   <Icon
                     name="question circle outline"
-                    style={{ color: '#556AEB' }}
+                    style={{ color: "#556AEB" }}
                   />
                   Which question is asked before and after the task
                 </div>
@@ -158,22 +171,22 @@ class CollaboratorsModal extends Component {
               <p>Default parameter values (can clone task and modify these)</p>
               <div className="symbolBlock">
                 <div>
-                  <Icon name="clone outline" style={{ color: '#556AEB' }} />
+                  <Icon name="clone outline" style={{ color: "#556AEB" }} />
                   <strong>60 trials</strong>
                 </div>
                 <div>
                   <Icon
                     name="question circle outline"
-                    style={{ color: '#556AEB' }}
+                    style={{ color: "#556AEB" }}
                   />
                   Show question every <strong>5 trials</strong>
                 </div>
                 <div>
-                  <Icon name="star outline" style={{ color: '#556AEB' }} />
+                  <Icon name="star outline" style={{ color: "#556AEB" }} />
                   Participants start the task with <strong>500 points</strong>
                 </div>
                 <div>
-                  <Icon name="random" style={{ color: '#556AEB' }} />
+                  <Icon name="random" style={{ color: "#556AEB" }} />
                   <strong>Randomized trial sequence</strong> <br /> Note that
                   trials are balanced across the 3 conditions (lose/gain/mixed)
                 </div>
@@ -184,17 +197,19 @@ class CollaboratorsModal extends Component {
                 These data are automatically written to a csv file upon
                 completion of the task
               </p>
-              <ul>
+              <ul className="contentBlock">
                 <li>variable1</li>
                 <li>variable2</li>
                 <li>variable3</li>
               </ul>
             </div>
+
             <div className="rightPanel">
               <h2>Task Screenshot</h2>
-              <img />
+              <img className="contentBlock" />
+
               <h2>Resources</h2>
-              <ul>
+              <ul className="contentBlock">
                 <li>citation1</li>
                 <li>citation2</li>
                 <li>citation3</li>
@@ -217,4 +232,4 @@ class CollaboratorsModal extends Component {
   }
 }
 
-export default CollaboratorsModal;
+export default TaskModal;
