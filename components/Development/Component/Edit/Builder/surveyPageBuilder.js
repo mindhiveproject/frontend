2fc79209c07f2715674dcfcc900bec931672ef9a
Yury-Shevchenko <shevchenko_yury@mail.ru>
2022-12-06 20:25:57 +0100

    survey builder compatibility with old version 2

--- components/Development/Component/Edit/Builder/surveyPageBuilder.js
+++ components/Development/Component/Edit/Builder/surveyPageBuilder.js
@@ -27,16 +27,16 @@ class SurveyPageBuilder extends Component {
     let updatedItems;
     if (className === 'options' || className === 'items') {
       const updatedOptions = items
-        .filter(item => item.id === name)
+        .filter(item => item.id == name)
         .map(item => item[className]);
       const options = updatedOptions[0];
       options[id] = value;
       updatedItems = items.map(item =>
-        item.id === name ? { ...item, [className]: options } : item
+        item.id == name ? { ...item, [className]: options } : item
       );
     } else {
       updatedItems = items.map(item =>
-        item.id === name ? { ...item, [className]: value } : item
+        item.id == name ? { ...item, [className]: value } : item
       );
     }
     this.setState({
