a942d11a519ca642a12936ef2aa903d416c269f5
Yury-Shevchenko <shevchenko_yury@mail.ru>
2023-04-20 13:33:40 +0200

    hide visualize for non-admin

--- components/Develop/Page.js
+++ components/Develop/Page.js
@@ -8,12 +8,15 @@ import Review from "./Review/index";
 import CollectSection from "../Development/Study/Collect/index";
 // import DownloadSection from '../Development/Study/Download/index';
 // import VisualizeSection from "../DataViz2.0/index";
-import VisualizeSection from "../DataViz/index";
+import VisualizeSection from "../DataViz2.0/index";
 import AnalyzeSection from "../Development/Study/Analyze/index";
+import InDev from "./inDev";
 
 export default class Page extends Component {
   render() {
-    const { page } = this.props;
+    const { page, user } = this.props;
+    const isAdmin = user?.permissions.includes("ADMIN");
+
     return (
       <>
         {page === "proposal" && <Proposal {...this.props} />}
@@ -21,9 +24,12 @@ export default class Page extends Component {
         {page === "builder" && <Builder {...this.props} />}
         {page === "review" && <Review {...this.props} />}
         {page === "collect" && <CollectSection {...this.props} />}
-        {page === "visualize" && (
-          <VisualizeSection id={this.props?.study?.id} {...this.props} />
-        )}
+        {page === "visualize" &&
+          (isAdmin ? (
+            <VisualizeSection id={this.props?.study?.id} {...this.props} />
+          ) : (
+            <InDev />
+          ))}
         {page === "analyze" && <AnalyzeSection {...this.props} />}
       </>
     );
